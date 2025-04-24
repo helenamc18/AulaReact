b5d69f4197794ad1910fd7b94f26c0c181dcd7cc
Helena Maciel Costa <helenamacielcosta46@gmail.com>
2025-04-24 11:05:46 -0300

    2404

---
+++ boasvindas/src/Exemplo1.jsx
@@ -0,0 +1,10 @@
+export default function BoasVindas({nome})
+{
+    return(
+        <div>
+            <p>
+                Fala galera aqui é a/o {nome} seja bem vindo ao React!!
+            </p>
+        </div>
+    )
+}
