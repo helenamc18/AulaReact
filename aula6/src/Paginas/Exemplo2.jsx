0dd57b10673564752fee855aa6ab4b883e5c56b7
helenamc18 <helenamacielcosta46@gmail.com>
2025-08-28 09:08:57 -0300

    2808

--- aula6/src/Paginas/Exemplo2.jsx
+++ aula6/src/Paginas/Exemplo2.jsx
@@ -1,8 +1,50 @@
+import {Link} from 'react-router-dom';
+import { useState } from "react";
+
+
+export default function Exemplo2()
+{
+    const [numero1, setNumero1] = useState(number);
+    const [numero2,setNumero2] = useState(number);
+
+    function calcular()
+    {
+        let number = Number(numero1) ;
+
+        setResultado("Olá " + nome + " você já viveu " + dias + " dias ");
+    }
+    function limpar()
+    {
+        setNome("");
+        setIdade("");
+        setResultado("");
+    }
+
 export default function Exemplo2()
 {
     return (
         <div>
-            <h1>Estudo de Estados (useState)</h1>
+            <h1>Exemplo 2</h1>
+            <form>
+                <p>
+                   Digite o numero 1: <br />
+                   <input type="text" value={num} onChange={ (e) => setNumero1(e.target.value )} />
+                   </p>
+                   <p>
+                   Digite o numero 2: <br />
+                   <input type="text"  value={num} onChange={ (e) => setNumero2(e.target.value )} />
+                   </p>
+                   <p>
+                   <input type="button" value="Exemplo 2" onClick={calcular}/>
+                   <input type="button" value="Voltar"  onClick={voltar} />
+                   </p>
+                   <b>Resultados: </b> <br/>
+                       Nome: {nome} <br/>
+                       Idade: {idade}
+                   <p>
+        
+    
+            </form>
         </div>
     )
-}
+    
